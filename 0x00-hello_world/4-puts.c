ddf425df338a9f8e32fc86c14ace0f710a1c6978
Tenny21 <tennyudom@gmail.com>
2022-09-08 13:44:13 -0700

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - This is the body of the code
+ *
+ * Return: Must return an integr value always zero
+ */
+
 int main(void)
 {
   char str[] = "\"Programming is like building a multilingual puzzle";
