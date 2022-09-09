f450e06822457feab2743ad7908da7f668ec6873
Tenny21 <tennyudom@gmail.com>
2022-09-08 21:19:43 -0700

    PO

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * Return: must return an integer
  */
 int main(void)
-{
-	char str[] = '\"Programming is like building a multilingual puzzle";
+{	
+	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return 0;
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * Return: must return an integer value always zero.
  */
 int main(void)
-{
+{	
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
 }
