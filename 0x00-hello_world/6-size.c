15ec82fa80f1ef6cf1b0572e0ab89da541097fd0
Tenny21 <tennyudom@gmail.com>
2022-09-08 22:18:32 -0700

    yjo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,15 @@
 /**
  * main - this is the bvody of the code
  *
- * return: must return an integer
+ * Return: must return an integer always zero
  */
 int main(void)
 {
-	printf("Sze of a char: %i byte(s)\n",sizeof(char));
-	printf("Size of an int: %i byte(s)\n",sizeof(int));
-	printf("Size of a long: %i byte(s)\n",sizeof(long int));
-	printf("Size of long long int: %i byte(s)\n",sizeof(long long int));
-	printf("Size of float: %i byte(s)\n",sizeof(float));
+	printf("Sze of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long: %i byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
 
