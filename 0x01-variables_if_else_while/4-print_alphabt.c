bb99844cfa416a9e59cc2e4fe1db5d83fa566221
Tenny21 <tennyudom@gmail.com>
2022-09-10 03:55:03 -0700

    y

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,12 @@
 int main(void)
 {	
 	for (char c ='a'; c <= 'z'; c++)
-	{	
+	{
 		if ( c != 'e' && c != 'q')
-		{	
-			putchar( c );
+		{
+			putchar( c);
 		}
 	}
-	putchar('\n');
 	return (0);
 }
 
