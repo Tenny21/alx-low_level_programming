8ed781adae2d5e21886226d92565f605d19adbfe
Tenny21 <tennyudom@gmail.com>
2022-09-10 09:40:21 -0700

    y

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - must contain the body
+ *
+ * Return: must return a zero
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+
+					if (a != 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}	
+			}
+		}
+	}
+}
+
