96c320a9e96206e923eded700ea1865913327e0f
Tenny21 <tennyudom@gmail.com>
2022-09-10 07:55:18 -0700

    u

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - body of the code
+ *
+ * Return: must return an integer zero
+ */
+int main(void)
+{
+	int l ='0';
+
+	while (l <= '9')
+	{
+		putchar(1);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
+
