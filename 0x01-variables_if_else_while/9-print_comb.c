821ba3f08a0ee778c9d9142f836db96a54508931
Tenny21 <tennyudom@gmail.com>
2022-09-10 08:40:09 -0700

    T

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,15 @@
  * Return: must return zero
  */
 int main(void)
-{	
+{
 	int a;
 
 	for (a = 0; a < 10; a++)
-	{	
+	{
 		putchar(a + '0');
 
 		if (a != 9)
-		{	
+		{
 			putchar(',');
 			putchar(' ');
 		}
