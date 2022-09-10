533f8ef5c482b7bb7b0ec4d7346b56558419985a
Tenny21 <tennyudom@gmail.com>
2022-09-10 04:39:28 -0700

    u

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,16 @@
 /**
  * main - contains the body of the code
  *
+ * Description: prints lowercase alphabets
+ *
  * Return: must return an  integer zero
  */
+
 int main(void)
 {
 	for (char c = 'a'; c <= 'z'; c++)
 	{
-		putchar( c );
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
