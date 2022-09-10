35c920afc87e9688c05a1b2ecf43743bd5e83f41
Tenny21 <tennyudom@gmail.com>
2022-09-10 06:26:36 -0700

    I

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,12 @@
  * Return: must return an integer zero
  */
 int main(void)
-{	
+{
 	char e[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int l = 0;
 
 	while (e[l] != '\0' && e[l] != '\n')
-	{	
+	{
 		putchar(e[l]);
 		l++;
 	}
