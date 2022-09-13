5a461969f41054b785a22d7ed01d78cbc24c21da
Tenny21 <tennyudom@gmail.com>
2022-09-13 04:04:56 -0700

    Lower case

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** main - The body of the code
+ * Description: prints _putchar to the standard output
+ * Return: must return an integer
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+
+	putchar("c\n");
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet- pribnt lowercase alphabet
+ *
+ * main- body of the code
+ * Description: print lowercase alphabets
+ * Return: must return an integer zero
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+}
+
