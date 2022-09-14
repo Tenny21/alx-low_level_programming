3fbf2e0a9c2a252fadb19ff103bd905b0aa33ffb
Tenny21 <tennyudom@gmail.com>
2022-09-13 18:18:15 -0700

    h

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 
 /**
  * int _islower(int c) - checks if a character is lowercase
+ * @c: char type letter
  *
  * Return: returns an int 1
  */
@@ -11,8 +12,9 @@ int _islower(int c)
 {
 	if (c > 96 && c < 123)
 	{
-		return (1);
-	} else 
+			return (1);
+	}
+	else
 	{
 		return (0);
 	}
