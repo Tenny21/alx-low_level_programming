0e42381befb16265559ccd055398ce49d7dfc84b
Tenny21 <tennyudom@gmail.com>
2022-09-19 21:42:54 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes apointer and updates its value
+ * @n: int to point
+ *
+ * Return: returns an int
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
