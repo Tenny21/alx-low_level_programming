9d42f7b7ef88fc5f7ad7129fbe81d5bf405ea94a
Tenny21 <tennyudom@gmail.com>
2022-09-19 23:15:54 -0700

    putas half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - puts half of a string
+ * @str: the string to reference
+ * Return: returns 0
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
