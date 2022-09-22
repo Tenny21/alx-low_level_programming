6c939b8ccb9fd27072efe9bc246cb2e56c67510f
Tenny21 <tennyudom@gmail.com>
2022-09-22 10:10:44 -0700

    h

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	while (n > 0 && *src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-		n--;
-	}
-	*dest = '\0';
+	int dlen = 0, j = 0;
 
+	while (dest[dlen])
+		dlen++;
+	while (j < n && src[j])
+	{
+		dest[dlen] = src[j];
+		dlen++;
+		j++;
+	}
+	dest[dlen + n + 1] = '\0';
 	return (dest);
 }
 
