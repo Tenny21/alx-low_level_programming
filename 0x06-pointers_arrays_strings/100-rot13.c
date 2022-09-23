91271e743e5d5f789ae72b8202598dc0206c4b7f
Tenny21 <tennyudom@gmail.com>
2022-09-23 03:54:33 -0700

    updated

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,43 +3,46 @@
 
 /**
  * rot13 - ..
- * @s: ..
+ * @str: ..
  *
  * Return: ...
  */
-char *rot13(char *s)
+char *rot13(char *str)
 {
+	int indx1 = 0, indx2;
 
-	int a = 0;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
 
-	while (s[a])
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
 
+	while (str[indx1])
 	{
-
-		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
-
+		for (indx2 = 0; indx2 < 52; indx2++)
 		{
-
-			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
-
+			if (str[indx1] == alphabet[indx2])
 			{
-
-				s[a] -= 13;
-
+				str[indx1] = rot13key[indx2];
 				break;
-
 			}
-
-			s[a] += 13;
-
-			break;
-
 		}
-
-		a++;
-
+		indx1++;
 	}
-
-	return (s);
+	return (str);
 }
 
