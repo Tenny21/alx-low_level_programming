915daf676a41e494922f0a9000b181423284d3cf
Tenny21 <tennyudom@gmail.com>
2022-09-26 14:11:23 -0700

    first

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - prints the byte of the first part of a string
+ * @s: string
+ * @accept: ...
+ *
+ * Return: returns the number of byte of the in segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len, i, j;
+
+	for (i = 0; s[i] < '\0'; i++)
+	{
+		for (j = 0; accept[j] < '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	return (len);
+}
+
