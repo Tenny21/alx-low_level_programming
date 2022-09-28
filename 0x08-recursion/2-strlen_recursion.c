e3c1c06c748a4e4ca34f47e6b30ad6a156e1372c
Tenny21 <tennyudom@gmail.com>
2022-09-28 06:48:24 -0700

    updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,15 +8,11 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s)
+	if (*s != '\0')
 	{
-		len++;
-		_strlen_recursion(s + 1);
-		return (len);
+		return ( 1 + (_strlen_recursion(s + 1)));
 	}
 	else
-		return ('\n');
+		return (0);
 }
 
