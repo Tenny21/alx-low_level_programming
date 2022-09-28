e898266a29344b6b3e07dff6bcd3c606626d3960
Tenny21 <tennyudom@gmail.com>
2022-09-28 05:22:42 -0700

    print string reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: pointer to the string
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	int i, j;
+
+	i = -1;
+	j = 0;
+
+	if (s[i] == s[j])
+		_putchar('\n');
+	else
+	{
+		_putchar(s[i]);
+		i++;
+		_print_rev_recursion(s - 1);
+	}
+}
+
