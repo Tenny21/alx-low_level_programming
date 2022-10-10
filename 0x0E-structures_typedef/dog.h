d37ab73a85be70464ce938fccb90b144bf16ebb8
Tenny21 <tennyudom@gmail.com>
2022-10-10 10:25:41 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - about a dog
+ * @name: of the dog
+ * @age: of the dog
+ * @owner: of the dog
+ *
+ * Description: dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* #ifndef DOG_H */
+
