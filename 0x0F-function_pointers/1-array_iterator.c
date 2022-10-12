3d63b936e1b2b4e17c85647e3a880304f5b081b1
Tenny21 <tennyudom@gmail.com>
2022-10-12 05:55:06 -0700

    up

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,13 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned long int i;
+	if (array == NULL || action == NULL)
+		return;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
 }
 
