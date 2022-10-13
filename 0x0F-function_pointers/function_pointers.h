ed677c47cf79afa5dbbb9aaada60886f92896d68
Tenny21 <tennyudom@gmail.com>
2022-10-12 20:27:57 -0700

    functionp

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,6 @@
-#ifndef STDDEF_H
-#define STDDEF_H
-#endif
-
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
