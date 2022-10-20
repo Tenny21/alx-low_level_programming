84464a5bed46737a8837680930329b04094715ea
Tenny21 <tennyudom@gmail.com>
2022-10-20 16:03:06 -0700

    function to return the length of a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the num of elements in a list
+ * @h: const pointer of user-defined type
+ *
+ * Return: num of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num_of_elements = 1;
+	list_t *p;
+
+	p = h->next;
+	while (p != NULL)
+	{
+		num_of_elements++;
+		p = p->next;
+	}
+	return (num_of_elements);
+}
+
