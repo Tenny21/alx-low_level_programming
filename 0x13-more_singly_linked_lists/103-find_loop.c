cbdd8a04da293804ded4d7387959649465c8ec49
Tenny21 <tennyudom@gmail.com>
2022-10-25 21:02:16 -0700

    list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -16,41 +16,41 @@ size_t free_listint_safe(listint_t **h);
  */
 size_t looped_listint_count(listint_t *head)
 {
-listint_t *tortoise, *hare;
-size_t nodes = 1;
+	listint_t *tortoise, *hare;
+	size_t nodes = 1;
 
-if (head == NULL || head->next == NULL)
-return (0);
+	if (head == NULL || head->next == NULL)
+		return (0);
 
-tortoise = head->next;
-hare = (head->next)->next;
+	tortoise = head->next;
+	hare = (head->next)->next;
 
-while (hare)
-{
-if (tortoise == hare)
-{
-tortoise = head;
-while (tortoise != hare)
-{
-nodes++;
-tortoise = tortoise->next;
-hare = hare->next;
-}
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+		while (tortoise != hare)
+		{
+			nodes++;
+			tortoise = tortoise->next;
+			hare = hare->next;
+		}
 
-tortoise = tortoise->next;
-while (tortoise != hare)
-{
-nodes++;
-tortoise = tortoise->next;
-}
+		tortoise = tortoise->next;
+		while (tortoise != hare)
+		{
+			nodes++;
+			tortoise = tortoise->next;
+		}
 
-return (nodes);
-}
-tortoise = tortoise->next;
-hare = (hare->next)->next;
-}
+		return (nodes);
+		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
 
-return (0);
+	return (0);
 }
 
 /**
@@ -65,34 +65,35 @@ return (0);
  */
 size_t free_listint_safe(listint_t **h)
 {
-listint_t *tmp;
-size_t nodes, index;
+	listint_t *tmp;
+	size_t nodes, index;
 
-nodes = looped_listint_count(*h);
+	nodes = looped_listint_count(*h);
 
-if (nodes == 0)
-{
-for (; h != NULL && *h != NULL; nodes++)
-{
-tmp = (*h)->next;
-free(*h);
-*h = tmp;
-}
-}
-
-else
-{
-for (index = 0; index < nodes; index++)
-{
-tmp = (*h)->next;
-free(*h);
-*h = tmp;
+	if (nodes == 0)
+	{
+		for (; h != NULL && *h != NULL; nodes++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
 	}
 
-*h = NULL;
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (nodes);
 }
 
-h = NULL;
-
-return (nodes);
-}
